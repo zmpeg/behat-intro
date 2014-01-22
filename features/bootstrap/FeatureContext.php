e6933885130ed2e9be78d8a805fc65df001edf9d
zmpeg <zandstra.matt@gmail.com>
2014-01-22 09:06:54 -0700

    add steps to pass the first scenario

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -29,4 +29,41 @@ class FeatureContext extends BehatContext
     {
         // Initialize your context here
     }
+
+    /**
+     * @Given /^I am in a directory "([^"]*)"$/
+     */
+    public function iAmInADirectory($dir)
+    {
+        if(!file_exists($dir))
+            mkdir($dir);
+
+        chdir($dir);
+    }
+
+    /**
+     * @Given /^I have a file named "([^"]*)"$/
+     */
+    public function iHaveAFileNamed($file)
+    {
+        touch($file);
+    }
+
+    /**
+     * @When /^I run "([^"]*)"$/
+     */
+    public function iRun($command)
+    {
+        exec($command, $output);
+        $this->output = trim(implode("\n", $output));
+    }
+
+    /**
+     * @Then /^I should get:$/
+     */
+    public function iShouldGet(PyStringNode $string)
+    {
+        if((string)$string !== $this->output)
+            throw new Exception("Actual output is:\n" . $this->output);
+    }
 }
